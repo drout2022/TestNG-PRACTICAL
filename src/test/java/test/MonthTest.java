86ed700feaee5a832d7946226656e22f0656e8f8
debasmita <debasmitaro@gmail.com>
2022-08-02 01:14:59 -0500

    first commit

---
+++ src/test/java/test/MonthTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import page.Test101Page;
+import util.BrowserFactory;
+
+public class MonthTest {
+	
+	WebDriver driver;
+
+	@Test
+	public void test() throws InterruptedException {
+
+		driver = BrowserFactory.init();
+		Test101Page testObj = PageFactory.initElements(driver, Test101Page.class);
+
+		testObj.validateMonthDropdown();
+		Thread.sleep(4000);
+		BrowserFactory.tearDown();
+
+	}	
+
+}
